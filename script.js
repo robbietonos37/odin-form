1c1f90c71dd1a7c763b6cdb5444caa626ab07d51
robbietonos37 <109938015+robbietonos37@users.noreply.github.com>
2023-12-28 00:23:36 -0600

    Make sure passwords match

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 const submitButton = document.getElementById('submit-button');
+const password1 = document.getElementById('user_password');
+const password2 = document.getElementById('confirm_password');
+
+
 
 submitButton.addEventListener('click', (e) => {
-    console.log('go fuck yaself');
+    const password = password1.value;
+    const confirmPassword = password2.value;
+
+
+    console.log(password);
+
+    console.log(confirmPassword);
+    if (password !== confirmPassword) {
+        alert("Passwords don't match");
+        e.preventDefault();
+    }
 })
